a24cb1145b852df87b159ba9316a8843a834b271
Luka Králík <luka.kralik@gmail.com>
2023-06-07 23:08:39 +0200

    disconneciton handling

--- freefang/main.py
+++ freefang/main.py
@@ -2,6 +2,7 @@ import socket
 import json 
 import models
 import freefang_utils as utils
+import threading
 
 def create_game(playercap):
 	
@@ -10,7 +11,6 @@ def create_game(playercap):
 	
 	
 	print("Game created")
-	players = []
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #For testing only
 	game.socket = s
@@ -25,9 +25,23 @@ def create_game(playercap):
 		
 		game.players.append(p)
 		print(f"{p.name} has joined the game")
+		threading.Thread(target=player_connection, args=(game, p)).start()
 	game.gameloop()
 
-		
+def player_connection(game, player):
+    while True:
+        try:
+            cmd = player.connection.recv(1024).decode()
+            if not cmd:
+                break  # Empty message means the player disconnected
+
+            # Handle player commands as needed
+
+        except ConnectionResetError: # Unexpected disconnection
+            break
+
+    game.players.remove(player)
+    print(f"{player.name} has left the game")
 		
 def main():
 	print("Starting FreeFang server.")
--- freefang/models.py
+++ freefang/models.py
@@ -7,6 +7,9 @@ class Player:
 		self.alive = True
 		self.name = ""
 		self.connection = 0
+		self.voted_by = 0 # Number of people who voted for this player
+		self.voted = False
+
 
 class WWgame:
 	def __init__(self):
@@ -30,7 +33,25 @@ class WWgame:
 
 				noroles.pop(index)
 			
-			
+	def update_player_count(self):
+		num_players = len(self.players)
+		print(f"Number of present players: {num_players}")
+
+	def handle_disconnections(self):
+		disconnected_players = [] # Track multiple disconnections at a time
+		for player in self.players:
+			try:
+				player.connection.send(b"")  # Sending empty message to check connection status
+			except ConnectionResetError:
+				disconnected_players.append(player)
+
+		for player in disconnected_players:
+			self.players.remove(player)
+			print(f"{player.name} disconnected")
+
+		self.update_player_count()
+
+		
 	def gameloop(self):
 		print("Game starting")
 		self.distribute_roles()
@@ -38,5 +59,7 @@ class WWgame:
 		inputs = [self.socket] + [i.connection for i in self.players]
 		outputs = [i.connection for i in self.players]
 		msgqueues = {}
+
+		self.handle_disconnections()
 		
 	
