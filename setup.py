f827b8d9a2aa1dc5a5663d198f7431b9456bc835
Luka Králík <luka.kralik@gmail.com>
2023-06-07 22:06:21 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FreeFang',
+    version='0.1.0',
+    author='Solirs',
+    description='',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+    ],
+)
+
