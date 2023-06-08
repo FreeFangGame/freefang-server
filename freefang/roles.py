09a36c590f97eddc9e9d9848acc827c8c75d298a
Luka Králík <luka.kralik@gmail.com>
2023-06-08 10:03:12 +0200

    create structures for the voting sytem

--- freefang/roles.py
+++ freefang/roles.py
@@ -8,8 +8,23 @@ class Villager(Role):
 	def __init__(self):
 		super(Villager, self).__init__()
 		pass
-		
+
 class Werewolf(Role):
 	def __init__(self):
 		super(Werewolf, self).__init__()
 		pass
+
+class Vote:
+    def __init__(self):
+        self.sender = None
+        self.target = None
+
+class CitizenVote(Vote):
+    def __init__(self):
+        super(CitizenVote, self).__init__()
+        pass
+
+class WerewolfVote(Vote):
+    def __init__(self):
+        super(WerewolfVote, self).__init__()
+        pass
