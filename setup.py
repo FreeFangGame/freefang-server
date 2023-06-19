a871bd7c301c92c7b514457cea12314656b954e4
Solirs <zcauchemar@gmail.com>
2023-06-19 22:39:16 +0200

    Change author in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='FreeFang',
     version='0.1.0',
-    author='Solirs',
+    author='FreeFang Development Team',
     description='',
     packages=find_packages(),
     install_requires=[
