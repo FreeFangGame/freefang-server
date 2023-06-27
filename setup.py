d138348458bfcc055c4ea7e20721e50b24b8cf10
Solirs <zcauchemar@gmail.com>
2023-06-27 23:32:40 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FreeFang',
-    version='0.1.0',
+    version='0.1.1',
     author='FreeFang Development Team',
     description='A libre implementation of the Werewolf game, also known as Mafia.',
     packages=find_packages(),
